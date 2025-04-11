52ffa2af29740ee8596f9bcbfa3ec4399ebbfe2f
yourself01 <Jonas.struijk@gmail.com>
2025-04-11 07:38:41 +0200

    Create verzamelBD.meta.js

---
+++ verzamelBD.meta.js
@@ -0,0 +1,12 @@
+// ==UserScript==
+// @name         VerzamelBD
+// @namespace    http://tampermonkey.net/
+// @version      2025-04-05
+// @description  Maak het leven wat makkelijker voor jezelf:)
+// @author       -
+// @match        https://*.grepolis.com/game/*
+// @icon         https://gme.cyllos.dev/res/bdready.png
+// @grant GM_setValue
+// @grant GM_getValue
+// @grant unsafeWindow
+// ==/UserScript==
